53d29b8392838eec0443bd0591af6a873e811cbf
itsmonicabena <87464371+itsmonicabena@users.noreply.github.com>
2023-10-12 23:54:01 -0500

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.scss";
+import Layout from "@/components/Layout";
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import CreateTrip from "@/pages/CreateTrip";
+import Hotel from "@/pages/Hotel";
+import Flight from "@/pages/Flight";
+import Car from "@/pages/Car";
 
-import App from '@/App';
+// Create a router configuration
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "create-trip", element: <CreateTrip /> },
+      { path: "hotel", element: <Hotel /> },
+      { path: "flight", element: <Flight /> },
+      { path: "car", element: <Car /> },
+    ],
+  },
+]);
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
