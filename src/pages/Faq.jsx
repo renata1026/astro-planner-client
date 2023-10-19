173cb19b76d3e7e1b64d60469880fec584e07c27
Ronald Paek <ronaldpaek@gmail.com>
2023-10-18 19:02:39 -0700

    update: faq page

--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -7,52 +7,54 @@ const Faq = () => {
     <div className="basic-faq-container">
       <div className="faq-content">
         <h3 className="faq-header">Frequently asked questions</h3>
-        <Question title="What is Airtrav?" defaultOpen>
+        <Question title="What is Astro Planner?" defaultOpen>
           <p>
-            Airtrav is a trip planner that offers a platform to plan a
-            fully-customized, realistic itinerary for any city and town across
-            the world. Whenever you have to plan a trip, you have to take into
-            account the major five factors influence your trip planning. These
-            factors are- flights and transportation, hotels, attractions and
-            things to do, tours & activities and restaurants.
+            Astro Planner is a trip planner that offers a platform for users to
+            craft a fully-customized, realistic itinerary for any city and town
+            worldwide. When planning a journey, Astro Planner takes into account
+            the five major factors influencing trip planning: flights and
+            transportation, hotels, attractions and things to do, tours &
+            activities, and restaurants.
           </p>
         </Question>
         <Question title="How can I contact you?">
           <p>
-            You can reach out to our dedicated customer support team through the
-            "Contact Us" page on our website. Alternatively, you can send us an
-            email at support@airtrav.com or call our toll-free number at
-            1-800-123-4567. Our team is available 24/7 to assist you with any
-            queries.
+            How can I contact you? You can get in touch with our dedicated
+            customer support team via the "Contact Us" section on our website.
+            Alternatively, you can email us at support@astroplanner.com or call
+            us on our toll-free number at 1-800-123-4567. Our team is available
+            around the clock to help with any inquiries.
           </p>
         </Question>
         <Question title="When will my credit card be charged?">
           <p>
-            Once you finalize your itinerary and confirm the booking, your
-            credit card will be charged immediately. A detailed invoice will be
-            sent to your registered email address, ensuring complete
-            transparency. Please ensure you review all details before confirming
-            the booking.
+            As soon as you finalize your itinerary and give the nod for the
+            booking, your credit card will be charged right away. An itemized
+            invoice will be dispatched to the email address you've provided,
+            ensuring total transparency. Do make sure to peruse all the
+            specifics before giving your final confirmation.
           </p>
         </Question>
         <Question title="What does my quote include?">
           <p>
-            Your Airtrav quote provides a comprehensive breakdown of all costs
-            associated with your trip. This includes flight tickets, hotel
-            accommodations, transportation within the destination, any tours &
-            activities you've selected, and an estimate for meals. We strive for
-            transparency, so there are no hidden charges. However, personal
-            expenses and any unplanned activities are not included in the quote.
+            Your Astro Planner quotation offers a thorough breakdown of every
+            cost related to your trip. This encompasses airfare, hotel stays,
+            local transportation, any tours & activities you've opted for, and
+            an approximation for dining. Our commitment is to transparency, so
+            rest assured there are no hidden fees. However, do note that
+            personal expenditures and any spur-of-the-moment activities aren't
+            part of the quote.
           </p>
         </Question>
         <Question title="Are there any specials?">
           <p>
-            Absolutely! Airtrav constantly collaborates with partners worldwide
-            to bring exclusive deals and specials for our users. These can range
-            from discounted flight tickets, special hotel rates, or even unique
-            experiences in select cities. Make sure to check the "Specials"
-            section on our website or subscribe to our newsletter to stay
-            updated with the latest offers!
+            Of course! Astro Planner is always in tandem with global partners to
+            introduce exclusive promotions and offers for our clientele. These
+            might include slashed airfares, privileged hotel tariffs, or even
+            one-of-a-kind experiences in specific locales. Don't forget to
+            frequent the "Specials" segment on our website or become a
+            subscriber to our newsletter to remain in the loop about the
+            freshest deals!
           </p>
         </Question>
       </div>
